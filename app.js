9548b5a137880ce98e4c53f6e1e6ed98984b95e1
Petros Kapakos <petros.kapakos1@aexp.com>
2017-03-29 18:33:28 -0400

    added koa server

---
+++ app.js
@@ -0,0 +1,13 @@
+const Koa = require('koa');
+const Router = require('koa-route');
+const serve = require('koa-static');
+const send = require('koa-send');
+
+const app = new Koa();
+
+app.use(serve(`${__dirname}/dist`));
+
+app.use(Router.get('/', function* index() {
+  yield send(this, `${__dirname}/dist/index.html`);
+}));
+app.listen(3000);
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Routes from './router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import ReactDOM from 'react-dom';
+import Routes from './router';
 import styles from './styles/global.css';
 window.HOST = 'https://api.server.addr';
-import injectTapEventPlugin from "react-tap-event-plugin";
 injectTapEventPlugin();
 
-main()
-
 function main() {
-	ReactDOM.render(Routes, document.getElementById('app'))
+  ReactDOM.render(Routes, document.getElementById('app'));
 }
+
+main();
