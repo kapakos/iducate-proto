8ad4c9d11f59af5cb145d2229ba9d7d22da7db18
Petros Kapakos <petros.kapakos1@aexp.com>
2017-03-29 21:44:38 -0400

    added PORT variable

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const send = require('koa-send');
 
 const app = new Koa();
 
+const PORT = process.env.PORT || 7000;
+
 app.use(serve(`${__dirname}/dist`));
 
 app.use(Router.get('/', function* index() {
   yield send(this, `${__dirname}/dist/index.html`);
 }));
-app.listen(3000);
+
+app.listen(PORT);
