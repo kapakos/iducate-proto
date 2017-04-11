bd75aed5ec93fc3c156694480c4b37789448ddc1
Petros Kapakos <petros.kapakos1@aexp.com>
2017-04-10 23:49:16 -0400

    added Settings page and user data

---
+++ src/data/users.js
@@ -0,0 +1,13 @@
+import moment from 'moment';
+
+const users = [
+  {
+    id: 0,
+    firstName: 'Petros',
+    lastName: 'Kapakos',
+    dob: moment('19770116').toString(),
+    email: 'kapakospetros@gmail.com',
+  },
+];
+
+export default users;
